618e68f9537209f71bb775ef3a88be8a127eb35b
1968-501 <kimdongchan@1968-501ui-MacBookAir.local>
2026-01-10 15:51:41 +0900

    사이드에 4가직프레임 형성 완료

--- main.py
+++ main.py
@@ -3,8 +3,8 @@ from tkinter import messagebox
 
 ## 프레임 먼저 설정
 # 메인프레인와 보조 프레임들이 필요함
-# 메인 프레임:보조브레임 = 7:3 
-# 보조 프레임에는 상단부터
+# 메인 프레임:보조브레임 = 7:3 비율
+# 보조 프레임에는 상단부터 -> CRUD:결제:키패드:배출구 = 3:1:3:1 비율
     # 1. CRUD입력창 프레임(원하는 음식을 적기 위한 입력창)
     # 2. 결제 프레임 - 금액을 얼마나 넣었는지에 대한 공간, 결제 할 방법 선택하기
     # 3. 키패드 프레임 - 캐리드는 위에는 어떤 번호를 썼는지 확인할 수 있는 입력창이 필요함 (입력창은 = "disabled")
@@ -23,6 +23,25 @@ main_frm.pack(side="left", fill="y", expand=True)
 #사이드 프레임 형성
 side_frm = tk.Frame(root, width=250, bg = "white")
 side_frm .pack(side="left", fill = "y", expand=True)
+
+
+#######사이드 프레임 나누기#######
+
+#1. 사이드 프레임 -CURD 플레임 
+board_frm = tk.Frame(side_frm, width=250, bg="white")
+board_frm.pack(side="top", fill="y", expand=True)
+
+#2. 사이드 프레임 - 결제 프레임 
+payment_frm = tk.Frame(side_frm, width=250, bg="white")
+payment_frm.pack(side="top", fill="y", expand=True)
+
+#3. 사이드 프레임 - 키패드 프래임
+keypad_frm = tk.Frame(side_frm, width=250, bg="white")
+keypad_frm.pack(side="top", fill="y", expand=True)
+
+#4. 사이드 프레임 - 배출구 프레임
+output_frm = tk.Frame(side_frm, width=250, bg="white")
+output_frm.pack(side="top", fill="y", expand=True)
 
 
 root.mainloop()
