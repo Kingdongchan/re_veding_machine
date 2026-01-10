da284eecd52290a3a84988ada2308738e458d76f
1968-501 <kimdongchan@1968-501ui-MacBookAir.local>
2026-01-10 16:51:52 +0900

    함수에 담기

--- product.py
+++ product.py
@@ -4,67 +4,65 @@ import tkinter as tk
     # 20개의 프레임을 형성(4x5 형식)
     # 20개의 상품을 배치 -> 리스트안 딕셔너리 형태 -> {번호, 상품이름, 가격, 재고}
     
-root = tk.Tk()
-root.geometry("550x600")
-
-#20개의 상품 만들기
-prd = [
-    {"num":1 ,"product": "코카콜라" ,"price":1400 ,"stock":5 },
-    {"num":2 ,"product": "펩시콜라" ,"price":1200 ,"stock":5 },
-    {"num":3 ,"product": "미에로화이바","price":900 ,"stock":5 },
-    {"num":4 ,"product": "망고쥬스","price":1000 ,"stock":5 },    
-    {"num":5 ,"product": "갈아만든 배","price":1300 ,"stock":5 },
-    {"num":6 ,"product": "몬스터에너지","price":2200 ,"stock":5 },
-    {"num":7 ,"product": "코코팜","price":1300 ,"stock":5 },
-    {"num":8 ,"product": "포카리스웨트","price":1300 ,"stock":5 }, 
-    {"num":9 ,"product": "초코송이","price":1300 ,"stock":5 },
-    {"num":10 ,"product": "오레오","price":1600 ,"stock":5 },
-    {"num":11,"product": "화이트하임","price":1500 ,"stock":5 },
-    {"num":12 ,"product": "예감","price":1600 ,"stock":5 },
-    {"num":13 ,"product": "초코쿠키","price":1300 ,"stock":5 },
-    {"num":14 ,"product": "다이제 씬","price":1500 ,"stock":5 },
-    {"num":15 ,"product": "에너지 초코바","price":1300 ,"stock":5 },
-    {"num":16 ,"product": "빠다코코낫","price":1600 ,"stock":5 }, 
-    {"num":17 ,"product": "신라면","price":1300 ,"stock":5 },
-    {"num":18 ,"product": "참깨라면","price":1300 ,"stock":5 },
-    {"num":19 ,"product": "육개장 사발면","price":1300 ,"stock":5 },
-    {"num":20 ,"product": "불닭볶음면","price":1500 ,"stock":5 }
-]
+def main_prd (main_frm):
+    #20개의 상품 만들기
+    prd = [
+        {"num":1 ,"product": "코카콜라" ,"price":1400 ,"stock":5 },
+        {"num":2 ,"product": "펩시콜라" ,"price":1200 ,"stock":5 },
+        {"num":3 ,"product": "미에로화이바","price":900 ,"stock":5 },
+        {"num":4 ,"product": "망고쥬스","price":1000 ,"stock":5 },    
+        {"num":5 ,"product": "갈아만든 배","price":1300 ,"stock":5 },
+        {"num":6 ,"product": "몬스터에너지","price":2200 ,"stock":5 },
+        {"num":7 ,"product": "코코팜","price":1300 ,"stock":5 },
+        {"num":8 ,"product": "포카리스웨트","price":1300 ,"stock":5 }, 
+        {"num":9 ,"product": "초코송이","price":1300 ,"stock":5 },
+        {"num":10 ,"product": "오레오","price":1600 ,"stock":5 },
+        {"num":11,"product": "화이트하임","price":1500 ,"stock":5 },
+        {"num":12 ,"product": "예감","price":1600 ,"stock":5 },
+        {"num":13 ,"product": "초코쿠키","price":1300 ,"stock":5 },
+        {"num":14 ,"product": "다이제 씬","price":1500 ,"stock":5 },
+        {"num":15 ,"product": "에너지 초코바","price":1300 ,"stock":5 },
+        {"num":16 ,"product": "빠다코코낫","price":1600 ,"stock":5 }, 
+        {"num":17 ,"product": "신라면","price":1300 ,"stock":5 },
+        {"num":18 ,"product": "참깨라면","price":1300 ,"stock":5 },
+        {"num":19 ,"product": "육개장 사발면","price":1300 ,"stock":5 },
+        {"num":20 ,"product": "불닭볶음면","price":1500 ,"stock":5 }
+    ]
 
 
 
-#상품 숫자와 상품명만 출력
-for i in range(len(prd)):
-    number = prd[i]["num"]
-    product = prd[i]["product"]
-    prd_price = prd[i]["price"] 
-    num_prd = f"{number}. {product} {prd_price}원"
+    #상품 숫자와 상품명만 출력
+    for i in range(len(prd)):
+        number = prd[i]["num"]
+        product = prd[i]["product"]
+        prd_price = prd[i]["price"] 
+        num_prd = f"{number}. {product} {prd_price}원"
+        
+        # 20개의 제품 틀을 만들기 
+        r = i//4
+        c = i%4
+        
+        #상품을 담을 프레임 형성 
+        prd_frm = tk.Frame(main_frm, width=5, height=5, bg="red")
+        prd_frm.grid(row=r, column=c, sticky="nsew")
     
-    # 20개의 제품 틀을 만들기 
-    r = i//4
-    c = i%4
-    
-    #상품을 담을 프레임 형성 
-    prd_frm = tk.Frame(root, width=5, height=5, bg="red")
-    prd_frm.grid(row=r, column=c, sticky="nsew")
-   
-    #높이와 넓이를 만들기 위한 가중치 형성
-    prd_frm.grid_columnconfigure(0, weight=1)
-    prd_frm.grid_rowconfigure(0, weight=1)
-    
-    #프레임 안에 상품명을 담을 라벨 생성(하단에 배치)
-    prd_label = tk.Label(prd_frm, text=num_prd)
-    prd_label.grid(row=0, column=0, sticky="s")
+        #높이와 넓이를 만들기 위한 가중치 형성
+        prd_frm.grid_columnconfigure(0, weight=1)
+        prd_frm.grid_rowconfigure(0, weight=1)
+        
+        #프레임 안에 상품명을 담을 라벨 생성(하단에 배치)
+        prd_label = tk.Label(prd_frm, text=num_prd)
+        prd_label.grid(row=0, column=0, sticky="s")
 
-##공간을 모두 사용하기 위해 가중치 주는 명령어 사용
-#열에 대한 가중치
-for z in range(4):
-    root.grid_columnconfigure(z, weight=1)
+    ##공간을 모두 사용하기 위해 가중치 주는 명령어 사용
+    #열에 대한 가중치
+    for z in range(4):
+        main_frm.grid_columnconfigure(z, weight=1)
 
-#행에 대한 가중치
-for q in range(5):
-    root.grid_rowconfigure(q, weight=1)
-    
-    
+    #행에 대한 가중치
+    for q in range(5):
+        main_frm.grid_rowconfigure(q, weight=1)
+        
+        
 
-root.mainloop()
+    
