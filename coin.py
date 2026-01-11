c07a7a092b8e65d1c591d46fe7e61c6816b4345f
1968-501 <kimdongchan@1968-501ui-MacBookAir.local>
2026-01-11 17:25:53 +0900

    창 닫기 로직 형성

--- coin.py
+++ coin.py
@@ -5,7 +5,7 @@ root.title("결정 방식 선택")
 root.geometry("350x100")
 
 coin_type = [
-    "100", "500", "1000", "5000", "10000", "카드"
+    100, 500, 1000, 5000, 10000, "카드"
 ]
 
 # 가중치 주기
@@ -21,7 +21,33 @@ for i in range(len(coin_type)):
     
     c = coin_type[i]
     
-    btn = tk.Button(root, text=c, width=2, height=2)
+    btn = tk.Button(root, text=c, width=2, height=2, command=lambda idx=c : payment_type(idx))
     btn.grid(row=1, column=i)
+    
+#투입완료 버튼 
+ok_bnt = tk.Button(root, text="투입 완료", command=lambda:destory_window())
+ok_bnt.grid(row=2, column=5)
 
+#만약 카드를 누른다면 True와 Faluse로 구별함 (신용카드로 가정하고 만듬)
+#나머지 버튼을 누르면 버튼 속에 있는 숫자들을 계속해서 더해줌
+def payment_type(num):
+    
+    count = ent.get()
+    
+    if num == "카드":
+        ent.config(state="normal")
+        ent.insert(0, "True")
+        ent.config(state="disabled")
+        destory_window()
+        
+    else:
+        count = count + num
+        
+        ent.config(state="normal")
+        ent.insert(0, f"{count}")
+        ent.config(state="disabled")
+        
+def destory_window():
+    root.destroy()
+    
 root.mainloop()
