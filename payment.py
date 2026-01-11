7d96fbea02ca2b8026263989548b60476c8db45b
1968-501 <kimdongchan@1968-501ui-MacBookAir.local>
2026-01-11 17:43:23 +0900

    python3

--- payment.py
+++ payment.py
@@ -1,4 +1,6 @@
 import tkinter as tk
+
+import coin
 # 돈이 얼마나 들어왔는지에 대한 입력창이 필요함 
 ## 결제 버튼이 존재함
     #새로운 창에서는 100, 500, 1000, 5000, 10000, 카드, 입력완료이 있어야함
@@ -11,11 +13,11 @@ root.grid_rowconfigure(0, weight=1)
 root.grid_rowconfigure(1, weight=1)
 
 #입력창 형성
-ent = tk.Entry(root, width=25)
-ent.grid(row=0, column=0)
+insert_ent = tk.Entry(root, width=25)
+insert_ent.grid(row=0, column=0)
 
 #'결제 방식 선택' 버튼 형성
-bnt = tk.Button(root, text="결제 방법 선택")
+bnt = tk.Button(root, text="결제 방법 선택", command=lambda: coin.coin_window(insert_ent))
 bnt.grid(row=1, column=0, sticky="e")
 
 root.mainloop()
